5c7a129a427f544173cab28c634976edcf217028
Omarr-kh <omar1752003@gmail.com>
2024-01-05 10:16:15 -0800

    tests for db_storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,67 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+
+@unittest.skipIf(STORAGE_TYPE != 'db', 'skip if environ is not db')
+class TestCountGet(unittest.TestCase):
+    """testing Count and Get methods"""
+
+    @classmethod
+    def setUpClass(cls):
+        """sets up the class tests"""
+        storage.delete_all()
+        cls.state = State(name="California")
+        cls.city = City(state_id=cls.state.id,
+                     name="Francisco")
+        cls.user = User(email="omar@holbertonschool.com",
+                     password="test")
+        cls.place1 = Place(user_id=cls.user.id,
+                       city_id=cls.city.id,
+                       name="hello")
+        cls.place2 = Place(user_id=cls.user.id,
+                       city_id=cls.city.id,
+                       name="a house two")
+        cls.amenity1 = Amenity(name="Wifi")
+        cls.amenity2 = Amenity(name="Cable")
+        cls.amenity3 = Amenity(name="Bucket Shower")
+        objects = [cls.state, cls.city, cls.user, cls.place1, cls.place2, cls.amenity1, cls.amenity2, cls.amenity3]
+        for obj in objects:
+            obj.save()
+
+    def setUp(self):
+        """initializes new user for testing"""
+        self.state = TestCountGet.state
+        self.city = TestCountGet.city
+        self.user = TestCountGet.user
+        self.place1 = TestCountGet.place1
+        self.place2 = TestCountGet.place2
+        self.amenity1 = TestCountGet.amenity1
+        self.amenity2 = TestCountGet.amenity2
+        self.amenity3 = TestCountGet.amenity3
+
+    def test_allreloadsave(self):
+        """... checks if all(), save(), and reload function
+        in new instance.  This also tests for reload"""
+        total = 0
+        db_objs = storage.all()
+        for obj in db_objs.values():
+            for x in [self.state.id, self.city.id, self.user.id, self.place1.id]:
+                if x == obj.id:
+                    total += 1
+        self.assertTrue(total == 4)
+
+    def test_get_for_place(self):
+        """ checks get() """
+        temp = storage.get('Place', self.place1.id)
+        self.assertEqual(self.place1.id, temp.id)
+
+    def test_count_for_amenity(self):
+        """ checks count()t"""
+        count = storage.count('Amenity')
+        self.assertEqual(3, count)
+
+    def test_count_for_all(self):
+        """ checks count()"""
+        count_all = storage.count()
+        self.assertEqual(8, count_all)
